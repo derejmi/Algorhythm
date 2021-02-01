daafb8a8c3b159c5ee936ea1f1b1c7501efecbe6
derejmi <derejemireyigbe@gmail.com>
2021-02-01 12:49:27 +0000

    add room component and ability to fetch info from api backend

--- algorhythm/api/urls.py
+++ algorhythm/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, RetrieveRoom
 
 urlpatterns =[
-    path('room', RoomView.as_view())
+    path('room', RoomView.as_view()),
+    
+    path('get-room', RetrieveRoom.as_view())
 ]
---
+++ algorhythm/frontend/src/components/Room.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Room extends React.Component {
+  state = {
+    votes_for_skip: 2,
+    can_guests_pause: false,
+    is_host: false,
+    code: this.props.match.params.code,
+    host_email: "",
+  };
+
+  componentDidMount() {
+    const url = `http://localhost:8000/api/get-room?code=${this.state.code}`;
+    fetch(url)
+      .then((r) => r.json())
+      .then((room) =>
+        this.setState({
+          votes_for_skip: room.votes_for_skip,
+          can_guests_pause: room.can_guests_pause,
+          is_host: room.is_host,
+          host_email: room.email,
+        })
+      );
+  }
+
+  render() {
+    // let code = this.props.match.params.code;
+    return (
+      <div>
+        <h1>Room - {this.state.code}</h1>
+        <p>Votes to skip songs: {this.state.votes_for_skip}</p>
+        <p>Can Guests Pause: {String(this.state.can_guests_pause)}</p>
+        <p>Host: {String(this.state.is_host)}</p>
+        <p>Host Email:{String(this.state.host_email)} </p>
+      </div>
+    );
+  }
+}
+
+export default Room;
