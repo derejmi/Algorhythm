5c3dedc1701fa4037a1dc8d6729eca2cacb5e411
stephanie-ai <stephanie_cooper@hotmail.com>
2021-02-01 17:43:32 +0000

    remove email

--- algorhythm/algorhythm/settings.py
+++ algorhythm/algorhythm/settings.py
@@ -128,8 +128,6 @@ from .settings import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'getfutureproof.testing@gmail.com'
-EMAIL_HOST_PASSWORD = 'january2021'
 # EMAIL_PORT = 587
 EMAIL_PORT = 465
 # EMAIL_USE_TLS = False
