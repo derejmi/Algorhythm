fb7c4d676a3d47b670fb4f552966eb769a0d9006
Tempestx4 <mavindidzetonderai@yahoo.com>
2021-02-02 10:13:44 +0000

    changed link to Link in room page

--- algorhythm/frontend/src/components/RoomJoinPage.js
+++ algorhythm/frontend/src/components/RoomJoinPage.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import { TextField, Button, Grid, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-
 export default class RoomJoinPage extends Component {
   constructor(props) {
     super(props);
-    this.state ={
-      roomCode = "",
-      error: ""
+    this.state = {
+      roomCode: "",
+      error: "",
     };
     this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
     this.roomButtonPressed = this.roomButtonPressed.bind(this);
@@ -16,7 +15,7 @@ export default class RoomJoinPage extends Component {
 
   render() {
     return (
-      <Grid container spacing={1} alignItems="center"> 
+      <Grid container spacing={1} alignItems="center">
         <Grid item xs={12}>
           <Typography variant="h4" component="h4">
             Join a Room
@@ -24,52 +23,56 @@ export default class RoomJoinPage extends Component {
         </Grid>
         <Grid item xs={12}>
           <TextField
-          error={this.state.error}
-          label="Code"
-          placeholder="enter a room code"
-          value={this.state.roomCode}
-          helperText={this.state.error}
-          variant="outlined"
-          onChange={this.handleTextFieldChange}
+            error={this.state.error}
+            label="Code"
+            placeholder="enter a room code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+            onChange={this.handleTextFieldChange}
           />
         </Grid>
-        <Grid item xs={12} align='center'>
-          <Button variant="contained" color="primary" onClick={this.roomButtonPressed}>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.roomButtonPressed}
+          >
             Enter Room
           </Button>
         </Grid>
-        <Grid item xs={12} align='center'>
-        <Button variant="contained" color="secondary" to="/" component={link}>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" to="/" component={Link}>
             Back
           </Button>
-          </Grid>
+        </Grid>
       </Grid>
     );
   }
   handleTextFieldChange(e) {
     this.setState({
-      roomCode: e.target.value
-    })
+      roomCode: e.target.value,
+    });
   }
   roomButtonPressed() {
     console.log(this.state.roomCode);
     const requestOptions = {
-      method: 'POST',
-      headers: {'content-type': 'application/JSON'},
+      method: "POST",
+      headers: { "content-type": "application/JSON" },
       body: JSON.stringify({
-        code:this.state.roomCode,
-      })
+        code: this.state.roomCode,
+      }),
     };
-    fetch('/api/join-room', requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        this.props.history.push('/room/${this.state.roomCode}');
-      } else { 
-        this.setState({error: 'Room not found'});
-      }
-    })
-    .catch((error) => {
-    console.log(error)
-    });
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          this.props.history.push(`/room/${this.state.roomCode}`);
+        } else {
+          this.setState({ error: "Room not found" });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
