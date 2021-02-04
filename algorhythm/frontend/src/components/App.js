b06a11e2c6bec54c592fb39c8a2822a78b1eed32
derejmi <derejemireyigbe@gmail.com>
2021-02-04 10:47:19 +0000

    setup testing

--- algorhythm/frontend/src/components/App.js
+++ algorhythm/frontend/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
   }
@@ -16,6 +16,8 @@ export default class App extends Component {
     );
   }
 }
+
+export default App;
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
