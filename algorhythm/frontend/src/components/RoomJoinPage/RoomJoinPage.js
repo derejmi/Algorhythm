864101af926733910c1c4d07ed8be70aa8b057fb
stephanie-ai <stephanie_cooper@hotmail.com>
2021-02-05 10:30:21 +0000

    file structure

---
+++ algorhythm/frontend/src/components/RoomJoinPage/RoomJoinPage.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: "",
+      error: "",
+    };
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    this.roomButtonPressed = this.roomButtonPressed.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="mainPage white">
+        <div>
+          <h1 >Join a Room</h1>
+        </div>
+        <div>
+          <form>
+            <label>Enter a Room Code
+            <input
+              // placeholder="enter a room code"
+              label="Code"
+              value={this.state.roomCode}
+              helperText={this.state.error}
+              onChange={this.handleTextFieldChange}
+              error={this.state.error}
+            /></label>
+          </form>
+        </div>
+        <div align="center">
+          <button className="joinSubmit" onClick={this.roomButtonPressed}>Join Room</button>
+        </div>
+        <div align="center">
+          <Link to="/">
+            <button>Back</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  handleTextFieldChange(e) {
+    this.setState({
+      roomCode: e.target.value,
+    });
+  }
+  roomButtonPressed() {
+    console.log(this.state.roomCode);
+    const requestOptions = {
+      method: "POST",
+      headers: { "content-type": "application/JSON" },
+      body: JSON.stringify({
+        code: this.state.roomCode,
+      }),
+    };
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          this.props.history.push(`/room/${this.state.roomCode}`);
+        } else {
+          this.setState({ error: "Room not found" });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
